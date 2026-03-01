7551201de09eeeb6b133b5d803c85076fcadfe85
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2026-03-01 20:17:36 +0900

    change load process to use custom tokenizer

--- codes/otp/models/_models.py
+++ codes/otp/models/_models.py
@@ -6,14 +6,26 @@ from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2CTCTokenizer, Wav2Vec
 
 def get_model(model_to="cpu"):
     model_id = "facebook/wav2vec2-xls-r-300m"
+    vocab_path = 'mod_vocab.json'
 
     feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_id)
 
-    tokenizer = Wav2Vec2CTCTokenizer.from_pretrained(model_id)
+    tokenizer = Wav2Vec2CTCTokenizer(
+        vocab_path,
+        unk_token="[UNK]",
+        pad_token="[PAD]",
+        word_delimiter_token="|"
+    )
 
     processor = Wav2Vec2Processor(feature_extractor=feature_extractor, tokenizer=tokenizer)
 
-    model = Wav2Vec2ForCTC.from_pretrained(model_id)
+    model = Wav2Vec2ForCTC.from_pretrained(
+        model_id,
+        ctc_loss_reduction="mean",
+        pad_token_id=processor.tokenizer.pad_token_id,
+        vocab_size=len(processor.tokenizer),
+        ignore_mismatched_sizes=True
+    )
     model.to(model_to)
 
     return model, processor
